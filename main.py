fabb365126e44513dfcd50eca3d9308f190c5f6d
aelias-sparta <aelias@spartaglobal.com>
2022-02-07 16:53:10 +0000

    updated csv code with formated dod, phone number, and invited both month and invited date is combine

--- main.py
+++ main.py
@@ -1,61 +1,23 @@
 import boto3
 from pprint import pprint as pp
 import pandas as pd
+from CSV_extractor import CSVextract
+from transformation import TransformTalent
 
-s3_client = boto3.client("s3")
-s3_resource = boto3.resource("s3")
 
+# #declare the file information
 bucket_name = 'data-26-final-project-files'
-prefix1 = 'Academy'
-prefix2 = 'Talent'
+# object_name = 'Academy'
+object_name= 'Talent'
 file_type = '.csv'
 
-academy_data = pd.read_csv('academy.csv')
-talent_data = pd.read_csv('talent.csv')
+csv_df = CSVextract(bucket_name=bucket_name, filetype=file_type, object_name=object_name)
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 
-print(f"\n********************* Talent Data  *****************************************\n")
-print(talent_data.info())
-print("\n")
-print(talent_data.isnull().sum())
-
-print(f"\n********************* academy Data  *****************************************\n")
-print(academy_data.info())
-print("\n")
-print(academy_data.isnull().sum())
-
-# bucket = s3_resource.Bucket(bucket_name)
-# contents = bucket.objects.all()
-# files = [item.key for item in contents]
-#
-# # combined_data = pd.concat(list_csv, join="outer", ignore_index=True)
-# # counter = 0
-# talent_list = []
-# academy_list = []
-#
-# for file in files:
-#     if '.csv' in file:
-#         if 'Talent' in file:
-#             #print(file)
-#             file_csv = s3_client.get_object(Bucket=bucket_name, Key=file)
-#             df = pd.read_csv(file_csv['Body'])
-#             talent_list.append(df)
-#         else:
-#             #print(file)
-#
-#             file_csv = s3_client.get_object(Bucket=bucket_name, Key=file)
-#             df = pd.read_csv(file_csv['Body'])
-#             academy_list.append(df)
-#
-# talent_data = pd.concat(talent_list, join="outer", ignore_index=True) # 12 csv files combined
-# academy_data = pd.concat(academy_list, join="outer", ignore_index=True) # this is 36 csv files combined
-#
-# print(f"\n********************* Talent Data  *****************************************\n")
-# print(talent_data.info())
-# print("\n")
-# print(talent_data.isnull().sum())
-#
-# print(f"\n********************* academy Data  *****************************************\n")
-# print(academy_data.info())
-# print("\n")
-# print(academy_data.isnull().sum())
-
+print("\n*************************** Talent Transformation ***********************************\n")
+# csv_df.extract()
+df = csv_df.localCSV()
+df = TransformTalent(df).df
+print(df.head(10))
---
+++ test_CSV_extractor.py
@@ -0,0 +1,28 @@
+from CSV_extractor import CSVextract
+import unittest
+
+class TestExtractCSV(unittest.TestCase):
+
+    def test_academy(self):
+        test_academy = CSVextract('data-26-final-project-files', 'Academy')
+
+        assert isinstance(test_academy.csv_file_names, list)
+        assert isinstance(test_academy.df, pd.DataFrame)
+        assert 'Academy/Business_21_2019-03-18.csv' in test_academy.csv_file_names
+        assert len(test_academy.csv_file_names) == 36
+        assert len(test_academy.df) == 397
+        assert len(test_academy.df.columns) == 62
+
+    def test_talent(self):
+        test_talent = ExtractCSV('data-26-final-project-files', 'Talent')
+
+        assert isinstance(test_talent.df, pd.DataFrame)
+        assert isinstance(test_talent.csv_file_names, list)
+        assert 'Talent/April2019Applicants.csv' in test_talent.csv_file_names
+        assert len(test_talent.csv_file_names) == 12
+        assert len(test_talent.df) == 4691
+        assert len(test_talent.df.columns) == 14
+
+
+if __name__ == '__main__':
+    unittest.main()
